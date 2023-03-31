cfc39b8c8d05b771a5547339a4e24abece38e180
MånsMårlid <mans99@live.se>
2023-03-31 13:19:43 +0200

    removed more unused code

--- app.js
+++ app.js
@@ -3,29 +3,19 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-const { Canvas, createCanvas, Image, ImageData, loadImage } = require("canvas");
-const { writeFileSync, existsSync, mkdirSync } = require("fs");
+const { createCanvas, loadImage } = require("canvas");
+const { writeFileSync, } = require("fs");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-const Console = require("console");
+
 const cv = require("opencv-sigge");
-const adaptiveThreshold = require("adaptive-threshold");
-const fs = require("fs");
+
 var app = express();
-const savePixels = require("save-pixels");
-const getPixels = require("get-pixels");
-var frame = new cv.Mat();
-var grey = new cv.Mat();
-const Jimp = require("jimp");
-const { JSDOM } = require("jsdom");
-const dom = new JSDOM();
-const document = dom.window.document;
 
-const HTMLCanvasElement = Canvas;
 
-const HTMLImageElement = Image;
+
 const wait = async () => {
-  const HTMLImageElement = Image;
+
   console.log("start");
   /*await loadOpenCV();*/
 
@@ -36,7 +26,6 @@ const wait = async () => {
   cv.cvtColor(src, gray, cv.COLOR_BGR2GRAY, 0);
   let thresh = new cv.Mat();
   cv.threshold(gray, thresh, 107, 980, cv.THRESH_BINARY);
-  let denoised = new cv.Mat();
   const canvas = createCanvas(200, 200);
   cv.imshow(canvas, thresh);
   writeFileSync("output.jpg", canvas.toBuffer("image/jpeg"));
