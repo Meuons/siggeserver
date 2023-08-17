141e6bcc496f7e6ebd9374217560f2b3b6fbc704
MånsMårlid <mans99@live.se>
2023-08-17 15:53:08 +0200

    image fix

--- routes/currencies.js
+++ routes/currencies.js
@@ -39,7 +39,7 @@ router.post("/read", function (req, res, next) {
                 "SELECT * FROM currencies",
                 (row) => {
                     if (row) {
-                        res.send(JSON.stringify({ message: "Read success", accounts: row }));
+                        res.send(JSON.stringify({ message: "Read success", currencies: row }));
                     } else {
                         res.send(JSON.stringify({ error: "No data found" }));
                     }
@@ -56,10 +56,10 @@ router.post("/read-single", function (req, res, next) {
     auth.authorize(body.token, body.username, true, (authorized) => {
         if (authorized) {
             db.get(
-                "SELECT * FROM currencies WHERE account_id = " + body.account_id,
+                "SELECT * FROM currencies WHERE currency_id = " + body.currency_id,
                 (row) => {
                     if (row) {
-                        res.send(JSON.stringify({ message: "Read success", account: row[0] }));
+                        res.send(JSON.stringify({ message: "Read success", currency: row[0] }));
                     } else {
                         res.send(JSON.stringify({ error: "No data found" }));
                     }
@@ -77,17 +77,12 @@ router.post("/delete", function (req, res, next) {
     auth.authorize(body.token, body.username, true, (authorized) => {
         if (authorized) {
            
-            db.get("SELECT EXISTS ( SELECT 1 FROM categories WHERE account = " + body.account_id + ") AS id_used_in_fk",
-            (row) => {
-                if (row) {
-                    if(row[0].id_used_in_fk == 0){
+           
+                   
 
-                    db.get("SELECT EXISTS ( SELECT 1 FROM receipts WHERE account = " + body.account_id + ") AS id_used_in_fk",
-                    (row) => {
-                        if(row){
-                            if(row[0].id_used_in_fk == 0){
+                        
                                 db.run(
-                                    "DELETE FROM currencies WHERE account_id = '" + body.account_id + "'",
+                                    "DELETE FROM currencies WHERE currency_id = '" + body.currency_id + "'",
                                     (err) => {
                                         if (err) {
                                             res.send(JSON.stringify({ error: `delete error: ${err}` }));
@@ -96,22 +91,10 @@ router.post("/delete", function (req, res, next) {
                                         }
                                     }
                                 );
-                            } else{
-                                res.send(JSON.stringify({ error: "Cannot delete record used in constraint" }));
-                            }
-                        }
                           
-                    
-                        }
-                   
-                    )
-                } else{
-                    
-                    res.send(JSON.stringify({ error: "Cannot delete record used in constraint" }));
-                }
-                }
-            }
-            )
+                      
+                  
+                
         
         } else {
             res.send(JSON.stringify({ error: "unauthorized" }));
@@ -126,16 +109,12 @@ router.post("/update", function (req, res, next) {
     const body = req.body;
     auth.authorize(body.token, body.username, true, (authorized) => {
         if (authorized) {
-            console.log(     "UPDATE currencies SET title =" +
-                "'" + body.title +
-                "' WHERE account_id = " +
-                body.account_id +
-                "")
+       
             db.run(
                 "UPDATE currencies SET title =" +
                 "'" + body.title +
-                "' WHERE account_id = " +
-                body.account_id +
+                "' WHERE currency_id = " +
+                body.currency_id +
                 "",
                 (err) => {
                     if (err) {
